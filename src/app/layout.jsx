80568cba1445650430ebf7e595f48d7e42d375eb
elvxk <pradiptaramadan9c@gmail.com>
2025-03-01 02:22:20 +0700

    update(title): update site name

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,7 +2,7 @@ import { inter, ceraRoundPro } from "@/utils/fonts";
 import "./globals.css";
 
 export const metadata = {
-  title: "SPLITBILL",
+  title: "Split Bill's",
   icons: {
     icon: "/favicon.ico",
   },
@@ -12,7 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${inter} ${ceraRoundPro} antialiased bg-bg`}>
-        <div className="grid min-h-[100dvh] grid-rows-[auto_1fr_auto] mx-10">
+        <div className="grid min-h-screen grid-rows-[auto_1fr_auto] mx-10">
           <main>{children}</main>
         </div>
       </body>
